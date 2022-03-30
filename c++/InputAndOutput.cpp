d78bc1c86c916819a57a3ef74dd8950b747d4c60
vijaykgopu <96440966+vijaykgopu@users.noreply.github.com>
2022-03-30 17:23:46 +0530

    Input and Output

---
+++ c++/InputAndOutput.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n1;
+    int n2;
+    int n3;
+    
+    cin >> n1 >> n2 >> n3;
+    cout << (n1 + n2 + n3);
+    
+    return 0;
+}
