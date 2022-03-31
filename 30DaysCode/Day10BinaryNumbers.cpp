b4523f405214ec98699aa71cc65d630c8f06b784
vijaykgopu <96440966+vijaykgopu@users.noreply.github.com>
2022-03-31 09:52:48 +0530

    Day 10: Binary Numbers

---
+++ 30DaysCode/Day10BinaryNumbers.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int countSetBits(int n){
+    int count = 0;
+    int max  = 0;
+    
+    while(n){
+
+        if(n & 1){
+            count++;
+        }else{
+            if(max < count)
+                max = count;
+            count = 0;
+        }
+
+        n >>= 1;
+    }
+    
+    return (max > count) ? max : count; 
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    cout << countSetBits(n);
+    
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
