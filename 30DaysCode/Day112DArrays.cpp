a30b6d70a391a443faf07f758f094e56c68ea807
vijaykgopu <96440966+vijaykgopu@users.noreply.github.com>
2022-04-01 09:27:05 +0530

    Update Day112DArrays.cpp

--- 30DaysCode/Day112DArrays.cpp
+++ 30DaysCode/Day112DArrays.cpp
@@ -28,16 +28,25 @@ int main()
         }
     }
 
-    int max = 0;
+    int max;
+    bool set_max = true;
+
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
             int sum =  arr[i+0][j+0] + arr[i+0][j+1] + arr[i+0][j+2]
                                      + arr[i+1][j+1]
                      + arr[i+2][j+0] + arr[i+2][j+1] + arr[i+2][j+2];
+
+            if(set_max){
+                max = sum;
+                set_max = false;
+            }
+            
             if(max < sum)
                 max = sum;
         }
     }
+
     cout << max;
     
     return 0;
