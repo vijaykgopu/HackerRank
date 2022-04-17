e0545a2ed5ef3052783ca50e34914a7b5a4ec52b
vijaykgopu <96440966+vijaykgopu@users.noreply.github.com>
2022-04-17 11:02:19 +0530

    Update Day27Testing.cpp

--- 30DaysCode/Day27Testing.cpp
+++ 30DaysCode/Day27Testing.cpp
@@ -7,19 +7,18 @@
 using namespace std;
 
 int minimum_index(vector<int> seq) {
-    if (seq.empty()) {
+    if (seq.empty())
         throw invalid_argument("Cannot get the minimum value index from an empty sequence");
-    }
+ 
     int min_idx = 0;
     for (int i = 1; i < seq.size(); ++i) {
         if (seq[i] < seq[min_idx]) {
             min_idx = i;
         }
     }
+    
     return min_idx;
 }
-
-
 
 class TestDataEmptyArray{
     public:
